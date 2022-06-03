7cd41c6ff1ac1579b77b3ca0ad26b824601c02ca
plinioduartt <plinio.duartes@hotmail.com>
2022-06-03 19:41:14 -0300

    refactor: move files

--- src/domain/_commons/entity.ts
+++ src/domain/_commons/entity.ts
@@ -1,9 +1,22 @@
 import crypto from 'node:crypto'
+import TIsValidInstance from '../_protocols/isValidInstance'
 
 class Entity {
   protected id: string
-  protected constructor (id?: string) {
-    this.id = id || crypto.randomUUID()
+  protected constructor (args: any) {
+    this.id = args.id || crypto.randomUUID()
+  }
+
+  protected isValidInstance (): TIsValidInstance {
+    const propertyNames: string[] = Object.getOwnPropertyNames(this)
+    const errors: Array<string | null> = propertyNames
+      .map(property => this[property] != null ? null : `property ${property} is missing.`)
+      .filter(item => !!item)
+
+    return {
+      isValid: errors.length === 0,
+      errors
+    }
   }
 }
 
