e9d82c2b4e1bd899a0321e7340062aebea1a2108
plinioduartt <plinio.duartes@hotmail.com>
2022-06-03 17:06:25 -0300

    feat: add intlFormatter

---
+++ src/domain/_commons/currency/intlFormat.ts
@@ -0,0 +1,16 @@
+const decimals = Number(process.env.DECIMALS) || 2
+
+interface formatOptionsType {
+  locale: string
+  style: string
+  currency: string
+}
+
+const intlFormat = (formatOptions: formatOptionsType): Intl.NumberFormat => new Intl.NumberFormat(formatOptions.locale, {
+  style: formatOptions.style,
+  currency: formatOptions.currency,
+  minimumFractionDigits: decimals,
+  maximumFractionDigits: decimals
+})
+
+export default intlFormat
