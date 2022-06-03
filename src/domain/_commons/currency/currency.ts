f6caf55f892b2fb578867dc60875384ce1f53aeb
plinioduartt <plinio.duartes@hotmail.com>
2022-06-03 17:10:03 -0300

    feat: add currency class for validate decimal values

---
+++ src/domain/_commons/currency/currency.ts
@@ -0,0 +1,37 @@
+
+const decimals: number = Number(process.env.DECIMALS) || 2
+const factorInitialDigit: string = '1'
+const decimalWithFistDigitLength = decimals + 1
+const factorString: string = factorInitialDigit.padEnd(decimalWithFistDigitLength, '0')
+const factor: number = parseInt(factorString)
+
+class Currency {
+  private readonly _amount: number
+
+  /**
+   *
+   * @param amount Need to be passed as a decimal value
+   */
+  constructor (amount: number) {
+    this._amount = amount
+  }
+
+  /**
+   *
+   * @param amount
+   * @returns
+   */
+  private convertFloatToInteger (amount: number): number {
+    return amount * factor
+  }
+
+  public get value (): number {
+    return this.convertFloatToInteger(this._amount)
+  }
+
+  public get originalValue (): number {
+    return this._amount
+  }
+}
+
+export default Currency
